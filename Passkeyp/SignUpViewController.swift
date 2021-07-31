f4f8810ddca5624b431e7d1aa77f2e7a648c06d6
Silvana <sborgo@utexas.edu>
2021-07-31 11:16:24 -0500

    sign in storyboard done

--- Passkeyp/SignUpViewController.swift
+++ Passkeyp/SignUpViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var repeatPasswordField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        // check for fields filled
+        guard let user = userField.text, let password = passwordField.text, let passwordRepeat = repeatPasswordField.text, user.count > 0, password.count > 0, passwordRepeat.count > 0 else {
+            statusLabel.text = "Missing field(s)."
+            statusLabel.isHidden = false
+            return
+        }
+        
+        // check for field length
+        guard password.count >= 8 else {
+            statusLabel.text = "Password has to contain at least 8 characters."
+            statusLabel.isHidden = false
+            return
+        }
+        
+        // check for password match
+        guard password == passwordRepeat else {
+            statusLabel.text = "Passwords don't match."
+            statusLabel.isHidden = false
+            return
+        }
+        
+        // check for no previous user with same username
+    }
+    
     /*
     // MARK: - Navigation
 
