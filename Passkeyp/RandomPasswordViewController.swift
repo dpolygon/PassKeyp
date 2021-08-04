81749b8c36b2943874b29f122b0b2dae50deb164
Silvana <sborgo@utexas.edu>
2021-08-03 21:43:12 -0500

    random password functioning

--- Passkeyp/RandomPasswordViewController.swift
+++ Passkeyp/RandomPasswordViewController.swift
@@ -7,15 +7,57 @@
 
 import UIKit
 
-class RandomPasswordViewController: UIViewController {
+class RandomPasswordViewController: UITableViewController {
 
+    private let charsAvailable = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "1234567890", "!@#$%^&*()_+[]{}|=-~"]
+    private var charsEnabled : [String] = []
+    @IBOutlet weak var passwordText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        passwordText.text = ""
+    }
 
+    @IBAction func generatePressed(_ sender: Any) {
+        guard charsEnabled.count > 0 else {
+            return
+        }
+        let passwordLenght = 20
+        let password = String((0..<passwordLenght).map{_ in
+            let index = Int.random(in: 0..<charsEnabled.count)
+            return charsEnabled[index].randomElement()!
+        })
+        passwordText.text = password
+    }
+    
+    @IBAction func copyPasswordPressed(_ sender: Any) {
+        let pasteboard = UIPasteboard.general
+        pasteboard.string = passwordText.text
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // selection of password options
+        if indexPath.section == 1 {
+            if let cell = tableView.cellForRow(at: indexPath) {
+                // check for accessory
+                if cell.accessoryType == .checkmark {
+                    cell.accessoryType = .none
+                    let index = charsEnabled.firstIndex(of: charsAvailable[indexPath.row])!
+                    charsEnabled.remove(at: index)
+                } else {
+                    cell.accessoryType = .checkmark
+                    charsEnabled.append(charsAvailable[indexPath.row])
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
